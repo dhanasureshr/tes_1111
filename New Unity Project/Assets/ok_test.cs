c12a6121cd83c50e429df70a087a350292b014b0
dhana suresh <dhanasuresh0002@gmail.com>
2019-12-19 01:12:37 +0530

    first_commit
    project_started_ok_dhana

---
+++ New Unity Project/Assets/ok_test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ok_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("hi dhana sureh you are sucess at version control");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
